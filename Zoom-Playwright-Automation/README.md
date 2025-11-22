cfe03111c72d8edbcded1bae97b84afa833f1aee
Md Dulare <isbahulhaq692@gmail.com>
2025-11-22 21:27:54 +0530

    Added Playwright Zoom Automation files

---
+++ Zoom-Playwright-Automation/README.md
@@ -0,0 +1,2 @@
+# Zoom Playwright Automation
+This project runs Brave + Playwright Zoom auto join bots in Colab.
---
+++ Zoom-Playwright-Automation/main.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import sync_playwright
+
+def run():
+    print("Playwright Working Successfully!")
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto("https://example.com")
+    print(page.title())
+    browser.close()
